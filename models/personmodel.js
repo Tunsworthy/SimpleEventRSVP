bbc71ec6adcf65714d9b592c67ddaecb5cd7c1e9
Tom Unsworth <mail@tomunsworth.net>
2019-09-08 13:33:10 +0200

    fixed validation on firstname lastname

--- models/personmodel.js
+++ models/personmodel.js
@@ -5,21 +5,18 @@ var fs = require('fs');
 var publickey = fs.readFileSync('./rsvppublic.pem').toString();
 
 function encrypt(text){
-  var buffer = Buffer.from(text);
-  var encrypted = crypto.publicEncrypt(publickey, buffer);
-  return encrypted.toString("base64");
+    var buffer = Buffer.from(text);
+    var encrypted = crypto.publicEncrypt(publickey, buffer);
+    return encrypted.toString("base64");
 };
 
 var PersonSchema = new Schema({
   firstname: {
     type: String,
-    required: 'firstname',
-  set: encrypt,
-
+    set: encrypt,
   },
   lastname: {
     type: String,
-    required: 'lastname',
     set: encrypt,
   },
   email: {
@@ -39,13 +36,5 @@ var PersonSchema = new Schema({
     default: 0
   },
 });
-var EncryptSchema = new Schema({
-  key:{
-    type:String
-  }
-});
-
-PersonSchema.set('toObject', { getters: true });
-PersonSchema.set('toJSON', { getters: true });
 
 module.exports = mongoose.model('Person', PersonSchema);
--- routes/allroutes.js
+++ routes/allroutes.js
@@ -9,26 +9,25 @@ app.get('/', function(req, res, next) {
   //req.flash('info', 'This is a test notification.');
   res.render('index', {title: 'Tom and Miao Qun\'s',subtitle:'Farewell Party',messages: req.flash('info'),error: req.flash('error'),
   	greeting: 'Dear friends, as you will know we are moving to Sydney. We have made many good friends over the years and are thankful to God for your friendship and support. We would love to see you a final time before we make our way down under and would be delighted if you can join us for our farewell party. The details are below; we will provide canapés and cakes but we kindly ask that you buy your own beverages. No need to bring any gifts or cards - your presence will be enough. We hope to see you there; either way, please keep in touch!'
-
 	})
 });
 
 
 app.post('/rsvp', function(req, res) {
-	var new_rsvp = new Person(req.body);
-		new_rsvp.validate(function(err,person){
-            if(err){
-            	console.log(err._message);
-            	req.flash('error','Sorry, there was an error - ' + err._message + ' Please enter Firstname and Lastname');
-            	res.redirect('/');
-            }else{
-	        new_rsvp.save(function(err,person){
-	        	req.flash('info', 'Thanks for your RSVP, see you soon!');
-				res.redirect('/');
-	        });	
-	    };
-          });
+  if(req.body.firstname == '' || req.body.lastname == ''){
+    req.flash('error','Please enter Firstname and Lastname');
+    res.redirect('/');
+  }else{
+    var new_rsvp = new Person(req.body);
+	  new_rsvp.save(function(err,person){
+	   if(err)
+      req.flash('error','Sorry there was an error, please try again');
+     req.flash('info', 'Thanks for your RSVP, see you soon!');
+     res.redirect('/');
+	    });
+    };
 });
+
 
 app.get('/rsvp', function(req,res){
   Person.find({},function(err, person){
